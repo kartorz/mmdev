e5b43068ac07a7a758881d7ab34b9c97cf152ca4
Ren Yu <renyu@nfschina.com>
2022-05-12 09:40:04 +0800

    alloc_differ_size_mem

--- mmcch.c
+++ mmcch.c
@@ -12,22 +12,80 @@
  *
  */
 
-cache_list(size_t size, kmem_cache *cache);
+#include <linux/slab.h>
+#include <linux/hashtable.h> // hashtable API
+#include <linux/module.h> 
+#include <linux/types.h> 
+
+DECLARE_HASHTABLE(tbl, 3);
+
+ struct kmem_cache* hash_cache;
+
+struct h_node {
+    struct kmem_cache* cache;
+    int size;
+    struct hlist_node node;
+};
+
+struct kmem_cache* cache_table(size_t size)
+{
+        struct h_node *cur;
+        hash_for_each_possible(tbl, cur, node, size) {
+                size, cur->data);
+
+                if (cur->size == size)) {
+                        return cur->cache;
+                }
+        }
+        return NULL;
+}
 
 struct kmem_cache *get_cache(char *name, size_t size)
 {
-        if (cache_list[size] == NULL) {        
+        struct kmem_cache * cache = cache_table(size);
+        if (cache == NULL) {        
                 char name[32];
+                h_node* hash_node = kmem_cache_alloc(hash_cache, GFP_KERNEL);
                 sprintf(name, "wine-object-%d", size);
-                struct kmem_cache *new =  kmem_cache_create(name, xxxx);
-                cache_list[size] = new;
+                hash_node->cache = kmem_cache_create(name,size, 0, SLAB_HWCACHE_ALIGN,NULL);
+                hash_node->size = size;
+
+                hash_add(tbl,&hash_node->node,size);
+                return hash_node->cache;
         } else {
-                return cache_list[size];
+                return cache;
         }
 }
 
 void* alloc_cache(size_t size)
 {
-        struct kmem_cache * = get_cache(size);
-        return kmem_cache_alloc(kmem_cache, gfp_mask);
+        struct kmem_cache* cache = get_cache(size);
+        return kmem_cache_alloc(cache, GFP_KERNEL);
+}
+
+void* cache_free(struct kmem_cache* cache,void* object)
+{
+        kmem_cache_free(cache,object);
+}
+
+void init_cache_for_hash(void)
+{
+  char name[32];
+  sprintf(name, "wine-cache-for-hash-%d",sizeof(h_node) );
+  struct kmem_cache *hash_cache =  kmem_cache_create(name, sizeof(struct h_node),0, SLAB_HWCACHE_ALIGN,NULL);
+}
+
+void exit(void)
+{    
+        struct h_node *cur;
+        unsigned bkt;
+
+        hash_for_each(tbl, bkt, cur, node) 
+        {
+                pr_info("myhashtable: element: data = %d",
+                cur->size, );
+                kmem_cache_free(hash_cache,cur);
+        }
+        kmem_cache_destroy(hash_cache);
+
 }
